c01ccfcc43d2954b99bcdf48394d46184e30003e
innerconsole <65080345+innerconsole@users.noreply.github.com>
2020-05-17 23:30:00 +0900

    Add files via upload

--- script/jquery.js
+++ script/jquery.js
@@ -1,21 +1,14 @@
-/* #menu */
-$('.menu').children(':button').on('click',function(){
-    if($(this).next('ul').children('li').css('display') == 'none'){
-        $(this).next('ul').children('li').show();
-    }else{
-        $(this).next('ul').children('li').hide();
-    }
-});
 
-/* search open */
-$('.search').find('button').on('click',function(){
-    $(this).hide();
-    $(this).next('.searchGroup').show();
-});
 
-/* search close */
-//$('.searchGroup').children('input[type=submit]').on('click',function(){
-$('.searchGroup').children(':submit').on('click',function(){
-    $(this).parent('.searchGroup').hide();
-    $(this).parent('.searchGroup').prev('button').show();
-});
+
+
+
+
+
+
+
+
+
+
+
+
