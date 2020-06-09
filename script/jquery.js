30f944c49112173ba62a09b5761bacf70206095f
innerconsole <65080345+innerconsole@users.noreply.github.com>
2020-06-10 00:25:23 +0900

    Add files via upload

--- script/jquery.js
+++ script/jquery.js
@@ -1,11 +1,12 @@
 $('.sideBt').on('click',function(){
-//    alert('메뉴 창 준비 중 입니다!');
     $('#side').show();
     return false;
 });
 
 
-
+$('.close').on('click',function(){
+    $('#side').hide();
+});
 
 
 
