2a425dfc4714b2461c84bb7f7b4f9e16254fcfce
innerconsole <65080345+innerconsole@users.noreply.github.com>
2020-06-11 12:16:26 +0900

    Add files via upload

--- script/jquery.js
+++ script/jquery.js
@@ -1,11 +1,22 @@
+/* sideOpen */
 $('.sideBt').on('click',function(){
-    $('#side').show();
+    $('#side').animate({
+        'right':'0'
+    },500);
+    $('body').append('<div id="dim"></div>',function(){
+        $('#dim').fadeIn();
+    });
     return false;
 });
 
-
+/* sideClose */
 $('.close').on('click',function(){
-    $('#side').hide();
+    $('#side').animate({
+        'right':'-220px'
+    },500);
+    $('#dim').fadeOut(function(){
+        $('#dim').remove();
+    });
 });
 
 
