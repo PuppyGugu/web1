6a87e4c2d52659a90840ba016779c2f83d005c1c
innerconsole <65080345+innerconsole@users.noreply.github.com>
2020-06-22 12:12:47 +0900

    Add files via upload

--- script/jquery.js
+++ script/jquery.js
@@ -44,7 +44,18 @@ $(document).on('click','#dim',function(){
 });
 
 /* main open */
-$('#main').children('.txt').fadeIn(2000);
+//$('#main').children('.txt').fadeIn(2000);
+
+$('#main').children('.txt').fadeIn(function(){
+    $(this).fadeOut(function(){
+        $(this).fadeIn(function(){
+            $(this).fadeOut(500,function(){
+                $(this).slideDown(500);
+            });
+        });
+    });
+});
+
 
 /* portfolio toggle */
 $('#more').on('click',function(){
