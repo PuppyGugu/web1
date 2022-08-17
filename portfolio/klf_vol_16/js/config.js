0206020c6271f0b76e3e5c20c6acde6a12f3ffc3
puppyGugu <65080345+puppyGugu@users.noreply.github.com>
2022-08-17 11:44:30 +0900

    피움 포폴 추가

---
+++ portfolio/klf_vol_16/js/config.js
@@ -0,0 +1,462 @@
+var bookConfig = {
+    //"FlipSound": "No",
+    "FlipSound": "Yes",
+    "QRCode": "Hide",
+    "appLogoIcon": "",
+    "appLogoLinkURL": "",
+    "HomeURL": "http://www.klf.or.kr/05_sub/page5_1.php",
+    //"appLogoOpenWindow": "Blank",
+    "appLogoOpenWindow": "Self",
+    "bookTitle": "FLIPBOOK",
+    "bookDescription": "",
+    "toolbarColor": "#8d9ed0",
+    "iconColor": "#ffffff",
+    "loadingCaptionColor": "#ffffff",
+    "loadingBackground": "#8d9ed0",
+    "logoHeight": 42,
+    "logoTop": 0,
+    "logoPadding": 0,
+    "HomeButtonVisible": "Show",
+    "ShareButtonVisible": "Show",
+    "ThumbnailsButtonVisible": "Show",
+    "thumbnailAlpha": 70,
+    "ZoomButtonVisible": "Show",
+    "FullscreenButtonVisible": "Show",
+    "TableOfContentButtonVisible": "Hide",
+    "SearchButtonVisible": "Show",
+    "leastSearchChar": 1, // 글자 입력 최소 "글자 길이가 1개 이상 되어야 합니다."
+    "PrintButtonVisible": "Show",
+    "printWatermarkFile": "",
+    "BookMarkButtonVisible": "Hide",
+    "BackgroundSoundButtonVisible": "Hide",
+    "BackgroundSoundURL": "",
+    "BackgroundSoundLoop": -1,
+    "HelpButtonVisible": "Hide",
+    "helpContentFileURL": "",
+    "helpWidth": 400,
+    "helpHeight": 518,
+    "showHelpContentAtFirst": "No",
+    "aboutButtonVisible": "Hide",
+    "aboutContactInfoTxt": "",
+    "AutoPlayButtonVisible": "Show",
+    "autoPlayAutoStart": "No",
+    "SlideshowAutoPlay": false,
+    "autoPlayDuration": 3,
+    "SlideshowPlayInterval": 5,
+    "autoPlayLoopCount": "1",
+    "minZoomWidth": 400,
+    "minZoomHeight": 518,
+    "mouseWheelFlip": "Yes",
+    "DownloadButtonVisible": "Hide",
+    "DownloadURL": "",
+    "UserInforSetting": "",
+    "bgBeginColor": "#e4e8f5",  // BG Start
+    "bgEndColor": "#e4e8f5",    // BG End
+    "bgMRotation": 90,
+    "backGroundImgURL": "",
+    "LeftShadowWidth": 100,
+    "LeftShadowAlpha": 1,
+    "RightShadowWidth": 40,
+    "RightShadowAlpha": 1,
+    "pageBackgroundColor": "#FFFFFF",
+    "flipshortcutbutton": "Show",
+    "OriginPageIndex": 1,
+    "RightToLeft": "No",
+    "flippingTime": 0.6,
+    "retainBookCenter": "Yes",
+    "FlipStyle": "Flip",
+    "totalPagesCaption": "",
+    "pageNumberCaption": "",
+    "topMargin": 10,
+    "bottomMargin": 10,
+    "leftMargin": 10,
+    "rightMargin": 10,
+    "LinkDownColor": "#0000FF",
+    "LinkAlpha": 0.4,
+    //"OpenWindow": "Blank",
+    "OpenWindow": "Self",
+    "googleAnalyticsID": "",
+    //"UIBaseURL": "\/\/static.fliphtml5.com\/book\/template\/Classical\/",
+    "UIBaseURL": ".\/",
+    "language": "English",
+    "AboutAddress": "",
+    "AboutAuthor": "",
+    "AboutEmail": "",
+    "AboutMobile": "",
+    "AboutWebsite": "",
+    "AboutDescription": "",
+    "totalPageCount": 52, // 총 페이지
+    "largePageWidth": 600,
+    "largePageHeight": 777,
+    "normalPath": "files\/large\/",
+    "largePath": "files\/large\/",
+    "thumbPath": "files\/thumb\/",
+    "VideoButtonVisible": "Hide",
+    "SlideshowButtonVisible": "Hide",
+    "addPaperCoil": "No",
+    "AnnotationButtonVisible": "Hide",
+    "CompanyLogoFile": "",
+    "bleedAreaLeft": "0%",
+    "bleedAreaRight": "0%",
+    "bleedAreaTop": "0%",
+    "bleedAreaBottom": "0%",
+    "HardPageEnable": "No",
+    "borderColor": "#562e0b",
+    "BindingType": "side",
+    "hardCoverBorderWidth": "8",
+    "outerCoverBorder": "Yes",
+    "cornerRound": "8",
+    "coverTexture": "none",
+    "iconFontColor": "#eeeeee",
+    "formBackgroundColor": "#8d9ed0",
+    "formFontColor": "#eeeeee",
+    "backgroundOpacity": "100",
+    "autoDoublePage": "Yes",
+    "searchKeywordFontColor": "#000000",
+    "backgroundPosition": "Stretch",
+    "CurlingPageCorner": "Yes",
+    "updatePageURL": "True",
+    "isTheBookOpen": "No",
+    "FlipDirection": "0",
+    "InstructionsButtonVisible": "Hide",
+    "showInstructionOnStart": "No",
+    "showThicknessOnMobile": "No",
+    "highDefinitionConversion": "No",
+    "pageNumColor": "#111111",
+    "backgroundScene": "None",
+    "TitleBarColor": "#0d6786",
+    "searchTextJS": "files\/search\/book_config.js",
+    //"searchPositionJS": "files\/search\/text_position[%d].js"
+};
+
+var fliphtml5_pages = [{
+    "l": "images/large/1.jpg",
+    "t": "images/thumb/1.jpg"
+}, {
+    "l": "images/large/2.jpg",
+    "t": "images/thumb/2.jpg"
+}, {
+    "l": "images/large/3.jpg",
+    "t": "images/thumb/3.jpg"
+}, {
+    "l": "images/large/4.jpg",
+    "t": "images/thumb/4.jpg"
+}, {
+    "l": "images/large/5.jpg",
+    "t": "images/thumb/5.jpg"
+}, {
+    "l": "images/large/6.jpg",
+    "t": "images/thumb/6.jpg"
+}, {
+    "l": "images/large/7.jpg",
+    "t": "images/thumb/7.jpg"
+}, {
+    "l": "images/large/8.jpg",
+    "t": "images/thumb/8.jpg"
+}, {
+    "l": "images/large/9.jpg",
+    "t": "images/thumb/9.jpg"
+}, {
+    "l": "images/large/10.jpg",
+    "t": "images/thumb/10.jpg"
+}, {
+    "l": "images/large/11.jpg",
+    "t": "images/thumb/11.jpg"
+}, {
+    "l": "images/large/12.jpg",
+    "t": "images/thumb/12.jpg"
+}, {
+    "l": "images/large/13.jpg",
+    "t": "images/thumb/13.jpg"
+}, {
+    "l": "images/large/14.jpg",
+    "t": "images/thumb/14.jpg"
+}, {
+    "l": "images/large/15.jpg",
+    "t": "images/thumb/15.jpg"
+}, {
+    "l": "images/large/16.jpg",
+    "t": "images/thumb/16.jpg"
+}, {
+    "l": "images/large/17.jpg",
+    "t": "images/thumb/17.jpg"
+}, {
+    "l": "images/large/18.jpg",
+    "t": "images/thumb/18.jpg"
+}, {
+    "l": "images/large/19.jpg",
+    "t": "images/thumb/19.jpg"
+}, {
+    "l": "images/large/20.jpg",
+    "t": "images/thumb/20.jpg"
+}, {
+    "l": "images/large/21.jpg",
+    "t": "images/thumb/21.jpg"
+}, {
+    "l": "images/large/22.jpg",
+    "t": "images/thumb/22.jpg"
+}, {
+    "l": "images/large/23.jpg",
+    "t": "images/thumb/23.jpg"
+}, {
+    "l": "images/large/24.jpg",
+    "t": "images/thumb/24.jpg"
+}, {
+    "l": "images/large/25.jpg",
+    "t": "images/thumb/25.jpg"
+}, {
+    "l": "images/large/26.jpg",
+    "t": "images/thumb/26.jpg"
+}, {
+    "l": "images/large/27.jpg",
+    "t": "images/thumb/27.jpg"
+}, {
+    "l": "images/large/28.jpg",
+    "t": "images/thumb/28.jpg"
+}, {
+    "l": "images/large/29.jpg",
+    "t": "images/thumb/29.jpg"
+}, {
+    "l": "images/large/30.jpg",
+    "t": "images/thumb/30.jpg"
+}, {
+    "l": "images/large/31.jpg",
+    "t": "images/thumb/31.jpg"
+}, {
+    "l": "images/large/32.jpg",
+    "t": "images/thumb/32.jpg"
+}, {
+    "l": "images/large/33.jpg",
+    "t": "images/thumb/33.jpg"
+}, {
+    "l": "images/large/34.jpg",
+    "t": "images/thumb/34.jpg"
+}, {
+    "l": "images/large/35.jpg",
+    "t": "images/thumb/35.jpg"
+}, {
+    "l": "images/large/36.jpg",
+    "t": "images/thumb/36.jpg"
+}, {
+    "l": "images/large/37.jpg",
+    "t": "images/thumb/37.jpg"
+}, {
+    "l": "images/large/38.jpg",
+    "t": "images/thumb/38.jpg"
+}, {
+    "l": "images/large/39.jpg",
+    "t": "images/thumb/39.jpg"
+}, {
+    "l": "images/large/40.jpg",
+    "t": "images/thumb/40.jpg"
+}, {
+    "l": "images/large/41.jpg",
+    "t": "images/thumb/41.jpg"
+}, {
+    "l": "images/large/42.jpg",
+    "t": "images/thumb/42.jpg"
+}, {
+    "l": "images/large/43.jpg",
+    "t": "images/thumb/43.jpg"
+}, {
+    "l": "images/large/44.jpg",
+    "t": "images/thumb/44.jpg"
+}, {
+    "l": "images/large/45.jpg",
+    "t": "images/thumb/45.jpg"
+}, {
+    "l": "images/large/46.jpg",
+    "t": "images/thumb/46.jpg"
+}, {
+    "l": "images/large/47.jpg",
+    "t": "images/thumb/47.jpg"
+}, {
+    "l": "images/large/48.jpg",
+    "t": "images/thumb/48.jpg"
+}, {
+    "l": "images/large/49.jpg",
+    "t": "images/thumb/49.jpg"
+}, {
+    "l": "images/large/50.jpg",
+    "t": "images/thumb/50.jpg"
+}, {
+    "l": "images/large/51.jpg",
+    "t": "images/thumb/51.jpg"
+}, {
+    "l": "images/large/52.jpg",
+    "t": "images/thumb/52.jpg"
+}
+
+];
+
+var language = [{
+    "language": "English",
+    "btnFirstPage": "처음",
+    "btnNextPage": "다음",
+    "btnLastPage": "끝",
+    "btnPrePage": "이전",
+    "btnGoToHome": "웹진",
+    "btnDownload": "다운로드",
+    "btnSoundOn": "소리 켜기",
+    "btnSoundOff": "소리 끄기",
+    "btnPrint": "프린트",
+    "btnThumb": "썸네일",
+    "btnBookMark": "북마크",
+    "frmBookMark": "북마크",
+    "btnZoomIn": "확대",
+    "btnZoomOut": "축소",
+    "btnAutoFlip": "자동 넘기기",
+    "btnStopAutoFlip": "자동 멈춤",
+    "btnSocialShare": "공유",
+    "btnHelp": "도움",
+    "btnAbout": "시작",
+    "btnSearch": "검색",
+    "btnFullscreen": "화면 전체 채움",
+    "btnExitFullscreen": "전체 화면 나가기",
+    "btnMore": "수량",
+    "frmPrintCaption": "프린트",
+    "frmPrintall": "전체 프린트 하기",
+    "frmPrintcurrent": "현재 페이지 프린트",
+    "frmPrintRange": "프린트 범위",
+    "frmPrintexample": "예) 2,3,5-10",
+    "frmPrintbtn": "프린트",
+    "frmShareCaption": "공유",
+    "frmShareLabel": "공유",
+    "frmShareInfo": "해당 매체를 소셜 네트워크에 쉽게 공유 할 수 있습니다. 아래 해당 버튼을 클릭하십시오.",
+    "frminsertLabel": "사이트에 첨부",
+    "frminsertInfo": "아래 코드를 사용하여이 매체를 웹 사이트에 포함하십시오.",
+    "frmaboutcaption": "접촉",
+    "frmaboutcontactinformation": "접촉 정보",
+    "frmaboutADDRESS": "주소",
+    "frmaboutEMAIL": "이메일",
+    "frmaboutWEBSITE": "웹사이트",
+    "frmaboutMOBILE": "모바일",
+    "frmaboutAUTHOR": "저자",
+    "frmaboutDESCRIPTION": "기술",
+    "frmSearch": "검색",
+    "frmToc": "목차",
+    "btnTableOfContent": "목차",
+    "btnNote": "주석",
+    "lblLast": "마지막 페이지 입니다.",
+    "lblFirst": "첫 페이지 입니다.",
+    "lblFullscreen": "전체화면으로 보려면 클릭하십시오.",
+    "lblName": "이름",
+    "lblPassword": "비밀번호",
+    "lblLogin": "로그인",
+    "lblCancel": "취소",
+    "lblNoName": "사용자 이름을 비워 둘 수 없습니다.",
+    "lblNoPassword": "비밀번로를 비워 둘 수 없습니다.",
+    "lblNoCorrectLogin": "사용자 이름과 비밀번호를 확인 하십시오.",
+    "btnVideo": "비디오 갤러리",
+    "btnSlideShow": "슬라이드 쇼",
+    "pnlSearchInputInvalid": "입력한 검색어가 너무 짧습니다.",
+    "btnDragToMove": "마우스 드래그로 이동",
+    "btnPositionToMove": "마우스 위치로 이동",
+    "lblHelp1": "페이지 모서리를 끌어서 보내십시오.",
+    "lblHelp2": "더블 클릭하여 확대 및 축소",
+    "lblCopy": "복사",
+    "lblAddToPage": "페이지에 추가",
+    "lblPage": "페이지",
+    "lblTitle": "제목",
+    "lblEdit": "편집",
+    "lblDelete": "삭제",
+    "lblRemoveAll": "전체 제거",
+    "tltCursor": "커서",
+    "tltAddHighlight": "하이라이트에 추가",
+    "tltAddTexts": "텍스트 추가",
+    "tltAddShapes": "도형 추가",
+    "tltAddNotes": "메모 추가",
+    "tltAddImageFile": "이미지 파일 추가",
+    "tltAddSignature": "서명 추가",
+    "tltAddLine": "라인 추가",
+    "tltAddArrow": "화살표 추가",
+    "tltAddRect": "사각형 추가",
+    "tltAddEllipse": "타원 추가",
+    "lblDoubleClickToZoomIn": "더블 클릭하면 확대됩니다.",
+    "lblPages": "페이지",
+    "infCopyToClipboard": "브라우저 용량이 클립 보드를 지원하지 않습니다. 직접 입력하십시오.",
+    "lblDescription": "제목",
+    "frmLinkLabel": "연결",
+    "infNotSupportHtml5": "브라우저가 HTML5를 지원하지 않습니다.",
+    "frmHowToUse": "사용하는 방법",
+    "lblHelpPage1": "책 페이지를 넘기려면 손가락을 움직입니다.",
+    "lblHelpPage2": "제스처를 사용하여 확대하거나 페이지를 두 번 클릭하십시오.",
+    "lblHelpPage3": "클릭하여 목차, 책갈피를보고 소셜 네트워크를 통해 책을 공유하십시오.",
+    "lblHelpPage4": "북마크를 추가하고 검색 기능을 사용하며 책을 자동으로 넘김니다.",
+    "lblHelpPage5": "썸네일을 열어 모든 책 페이지를 개요로 표시하십시오.",
+    "frmQrcodeCaption": "하단의 QR 코드를 스캔하여 휴대폰으로 봅니다.",
+    "btnPageBack": "뒤로",
+    "btnPageForward": "앞으로",
+    "btnLanguage": "언어 변경",
+    "msgConfigMissing": "구성 파일이 없어서 책을 열 수 없습니다."
+}];
+
+var pageEditor = {
+    "setting": {},
+    "pageAnnos": [
+        [], // 001
+        [], // 002
+        [], // 003
+        [], // 004
+        [], // 005
+        [], // 006
+        [], // 007
+        [], // 008
+        [], // 009
+        [], // 010
+        [], // 011
+        [], // 012
+        [], // 013
+        [], // 014
+        [], // 015
+        [], // 016
+        [], // 017
+        [], // 018
+        [], // 019
+        [], // 020
+        [], // 021
+        [], // 022
+        [], // 023
+        [], // 024
+        [], // 025
+        [], // 026
+        [], // 027
+        [], // 028
+        [], // 029
+        [], // 030
+        [], // 031
+        [], // 032
+        [], // 033
+        [], // 034
+        [], // 035
+        [], // 036
+        [], // 037
+        [], // 038
+        [], // 039
+        [], // 040
+        [], // 041
+        [], // 042
+        [], // 043
+        [], // 044
+        [], // 045
+        [], // 046
+        [], // 047
+        [], // 048
+        [], // 049
+        [], // 050
+        [], // 051
+        []  // 052
+    ]
+};
+var ols = [];
+var slideshow = [];
+var videoList = [];
+var bmtConfig = [   // 책갈피
+    //{ "caption": "첫페이지", "color": "#009786", "pageIndex": "1" },
+    //{ "caption": "5페이지", "color": "#ff0000", "pageIndex": "5" }
+];
+var staticAd = {};
+var flipByAudio = {};
+var bookPlugin = null;
+var phoneNumber = [];
+bookConfig.language = "English";
---
+++ portfolio/klf_vol_16/main.html
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="ko" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="author" content="user" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta property="og:type" content="website" />
+		<meta property="og:image" content="./images/sns/sns_img.jpg" />
+		<meta property="og:title" content="피움 36.5 KANGWONLAND WELFARE FOUNDATION MAGAZINE 2021. WINTER vol.16" />
+		<meta property="og:site_name" content="피움 36.5 KANGWONLAND WELFARE FOUNDATION MAGAZINE 2021. WINTER vol.16" />
+		<meta property="og:url" content="http://klf.or.kr/ebook/" />
+		<meta property="og:description" content="EMPATHY 2021년 <피움 36.5>는 우리 곁을 지켜준 ‘HERO’의 이야기를 담았습니다. Help(지원), Empathy(공감), Relationship(관계), Obligation(책임) 등 4가지 주제를 중심으로 각 호마다 폐광지역의 HERO를 소개합니다." />
+		<meta name="twitter:title" content="피움 36.5 KANGWONLAND WELFARE FOUNDATION MAGAZINE 2021. WINTER vol.16" />
+		<meta name="twitter:site" content="http://klf.or.kr/ebook/" />
+		<meta name="twitter:image" content="./images/sns/sns_img.jpg" />
+		<meta name="twitter:description" content="EMPATHY 2021년 <피움 36.5>는 우리 곁을 지켜준 ‘HERO’의 이야기를 담았습니다. Help(지원), Empathy(공감), Relationship(관계), Obligation(책임) 등 4가지 주제를 중심으로 각 호마다 폐광지역의 HERO를 소개합니다." />
+		<meta name="nate:title" content="피움 36.5 KANGWONLAND WELFARE FOUNDATION MAGAZINE 2021. WINTER vol.16" />
+		<meta name="nate:site_name" content="undefined" />
+		<meta name="nate:image" content="./images/sns/sns_img.jpg" />
+		<meta name="nate:url" content="http://klf.or.kr/ebook/" />
+		<meta name="nate:description" content="EMPATHY 2021년 <피움 36.5>는 우리 곁을 지켜준 ‘HERO’의 이야기를 담았습니다. Help(지원), Empathy(공감), Relationship(관계), Obligation(책임) 등 4가지 주제를 중심으로 각 호마다 폐광지역의 HERO를 소개합니다." />
+		<meta name="description" content="EMPATHY 2021년 <피움 36.5>는 우리 곁을 지켜준 ‘HERO’의 이야기를 담았습니다. Help(지원), Empathy(공감), Relationship(관계), Obligation(책임) 등 4가지 주제를 중심으로 각 호마다 폐광지역의 HERO를 소개합니다." />
+		<meta name="keywords" content="피움, 36.5, 이북, 2021, vol.16, 계간" />
+		<meta name="robots" content="none" />
+		<link href="./images/sns/sns_img.jpg" rel="image_src" />
+		<link href="./images/common/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+		<link href="./images/common/favicon.ico" type="image/x-icon" rel="icon" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>eBook : 피움 36.5 KANGWONLAND WELFARE FOUNDATION MAGAZINE 2021. WINTER vol.16</title>
+		<link rel="stylesheet" href="./css/phoneTemplate.css" />
+		<link rel="stylesheet" href="./css/style.css" />
+		<link rel="stylesheet" href="./css/player.css" />
+		<script src="./js/jquery-1.9.1.js"></script>
+		<script src="./js/head.js"></script>
+	</head>
+
+	<body class="wrapMain">
+		<script src="./js/config.js"></script>
+		<script src="./js/LoadingJS.js"></script>		
+		<script src='./js/main.js'></script>
+		<script src="./files/search/book_config.js"></script>
+		<script type="text/javascript">
+		//<![CDATA[
+			var textForPages = [];
+			var positionForPages = [];
+			disableSelection($("body")[0]);
+		//]]>
+		</script>
+		<link rel='stylesheet' href='./css/template.css' />
+		<script type="text/javascript">
+			var sendvisitinfo = function(type,page){};
+		</script>
+		<script src="./js/flipHtml5.hiSlider2.min.js"></script>
+		<link rel="stylesheet" href="./css/hiSlider2.css" />
+		<script src="./js/MovingBackgrounds.min.js"></script>
+		<link rel="stylesheet" href="./css/movingBackgrounds.css" />
+		<script src="./js/flipBookPlugins.js"></script>
+		<link rel="stylesheet" href="./css/flipBookPlugins.css" />
+		<script src="./js/slideJS.js"></script>
+	</body>
+</html>
